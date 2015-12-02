f3199f77040473f3ad7cad29271c6bb34268f9cc
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-12-01 23:40:20 -0700

    Added delete delete functionality.
    The view was changed so that the idea could be deleted and controller changed to include a destroy action.
    
    Next step will be to include change in idea quality.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -40,5 +40,16 @@ function deletePost(){
   $('#latest-ideas').delegate('#delete-idea', 'click', function(){
     var $post = $(this).closest(".idea")
     console.log($post.attr('data-id'));
+    $.ajax({
+      type: 'DELETE',
+      url:  '/api/v1/ideas/' + $post.attr('data-id') + '.json',
+      success: function(){
+        $post.remove()
+      },
+      error: function(){
+        $post.remove()
+        console.log('The post was already deleted.')
+      }
+    })
   })
 }
