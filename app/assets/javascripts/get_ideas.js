d6d9345f0f75e032b824e8433b95f37bfecf273b
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-12-03 02:35:45 -0700

    Completed base functionality.
    Added filter text area in html, filterIdeas function, and updated gitIdeas function with spans.
    There are no issues with filter, with the exception of when an idea quality is upped or downed user
    can see the page reload. Update functions and controller would need to be updated to only update portions
    of ideas that are being updated, instead of reloading page.
    
    The are still issues with the modal not closing and tests still need to be added.

--- app/assets/javascripts/get_ideas.js
+++ app/assets/javascripts/get_ideas.js
@@ -4,14 +4,14 @@ function getIdeas(){
     url:  '/api/v1/ideas.json',
     success: function(ideas){
       var ideaElements = $.map(ideas, function(idea, index){
-        var $ideaItem = $('<div></div>')
+        var $ideaItem = $('<span><div></div></span>')
         .attr( 'class', 'idea')
         .attr( 'data-id', idea.id)
         .append("<h3 class='title'>"+idea.title+"</h3>")
         .append("<p class='body'>"+idea.body+"</p>")
         .append("<p>"+idea.quality+'</p>')
-        .append("<button id='thumbs-up' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-up' aria-hidden='true'></span></button>")
-        .append("<button id='thumbs-down' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-thumbs-down' aria-hidden='true'></span></button></div>")
+        .append("<button id='thumbs-up' class='btn btn-default btn-sm'><div class='glyphicon glyphicon-thumbs-up' aria-hidden='true'></div></button>")
+        .append("<button id='thumbs-down' class='btn btn-default btn-sm'><div class='glyphicon glyphicon-thumbs-down' aria-hidden='true'></div></button></div>")
         .append("</p><button id='delete-idea' class='btn btn-default btn-m'>Delete Idea</button> ")
         .append("<button id='edit' type='button' class='btn btn-primary' data-toggle='modal' data-target='.bs-example-modal-lg'>Edit</button>");
         return $ideaItem;
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,4 +6,5 @@ $(document).ready(function(){
   thumbsDown();
   openEdit();
   updateIdea();
+  filterIdeas();
 })
